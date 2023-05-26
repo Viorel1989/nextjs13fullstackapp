fbc3a4dd6579ca60998e50035456c7e807418ea1
Viorel <viorel_popa89@yahoo.com>
2023-05-26 12:26:28 +0300

    update nextjs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["bcrypt"],
-    forceSwcTransforms: false,
   },
   typescript: {
     // !! WARN !!
